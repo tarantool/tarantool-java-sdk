881f8ab3d2c38ad5e7dce8ec71ca438322677976
Николай Белоногов <nikolai.belonogov@vk.team>
2026-01-15 13:53:04 +0300

    fix(spring-data-test): increase test timeout

--- tarantool-spring-data/tarantool-spring-data-34/src/test/java/io/tarantool/spring/data34/integration/BaseIntegrationTest.java
+++ tarantool-spring-data/tarantool-spring-data-34/src/test/java/io/tarantool/spring/data34/integration/BaseIntegrationTest.java
@@ -25,7 +25,7 @@ import static io.tarantool.spring.data34.utils.TarantoolTestSupport.writeTestPro
 import io.tarantool.spring.data34.config.properties.TarantoolProperties;
 
 @Testcontainers
-@Timeout(5)
+@Timeout(20)
 public abstract class BaseIntegrationTest {
 
   protected static TarantoolContainerOperations<?> clusterContainer;
